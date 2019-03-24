5856e1f549b3464c4b7959fc26364ceb93c1a8e5
Giuseppe Maggio <jertlok@protonmail.com>
2019-03-24 17:02:40 +0100

    Dreamliner: Initial import for SystemUI
    With this commit we introduce support for the pixel
    stand.
    
    Change-Id: I4bfb76618596a425237baab1d1eaad7223afd081
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/systemui/GoogleServices.java
+++ src/com/android/systemui/GoogleServices.java
@@ -4,9 +4,7 @@ import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.VendorServices;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.StatusBar;
-// import com.google.android.systemui.ambientmusic.AmbientIndicationContainer;
-// import com.google.android.systemui.ambientmusic.AmbientIndicationService;
-// import com.google.android.systemui.dreamliner.DockObserver;
+import com.google.android.systemui.dreamliner.DockObserver;
 import com.google.android.systemui.elmyra.ElmyraContext;
 import com.google.android.systemui.elmyra.ElmyraService;
 import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
@@ -33,5 +31,9 @@ public class GoogleServices extends VendorServices {
         if (new ElmyraContext(this.mContext).isAvailable()) {
             addService(new ElmyraService(this.mContext, new ServiceConfigurationGoogle(this.mContext)));
         }
+        // Dreamliner service
+        // TODO: do not start this service for every Pixel, check for a
+        // property or something.
+        addService(new DockObserver(mContext));
     }
 }
