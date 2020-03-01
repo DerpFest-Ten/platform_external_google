31e2a19a73f35473090cce189d2f8417b4d50214
ezio84 <brabus84@gmail.com>
2020-03-01 10:31:39 +0100

    Add kill app action to squeeze actions [3/3]
    Change-Id: I664c731b276293f3e0e0b94042e62db02ed9b550

--- SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
+++ SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -97,6 +97,11 @@ public class CustomActions extends Action {
             case 12: // Ringer modes
                 aosipUtils.toggleRingerModes(getContext());
                 break;
+            case 13: // Kill app
+                if (isScreenOn) {
+                    aosipUtils.killForegroundApp();
+                }
+                break;
         }
     }
 
