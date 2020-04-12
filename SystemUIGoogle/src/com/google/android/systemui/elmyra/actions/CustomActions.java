DirtyUnicorns/android_external_google

Active Edge & Gestures nav: Add Skip/Previous Song Function [3/3]
Change-Id: I4d5726cf8baf681b52b64ede1412cf842d3cea4e

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -9,6 +9,7 @@ import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 
 import com.android.internal.util.du.ActionUtils;
 import com.android.systemui.Dependency;
@@ -101,6 +102,12 @@ public class CustomActions extends Action {
                 if (isScreenOn) {
                     ActionUtils.killForegroundApp();
                 }
+                break;
+            case 14: // Skip song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }

DerpFest-Ten/platform_external_google

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -9,6 +9,7 @@ import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 
 import com.android.internal.util.derp.derpUtils;
 import com.android.systemui.Dependency;
@@ -101,6 +102,12 @@ public class CustomActions extends Action {
                 if (isScreenOn) {
                     derpUtils.killForegroundApp();
                 }
+                break;
+            case 14: // Skip song
+                derpUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                derpUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }